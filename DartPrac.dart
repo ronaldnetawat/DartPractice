8e28f0eb1aee6e5092b1c44fca2e92581c3c48ea
Ron Netawat <68713881+ronaldnetawat@users.noreply.github.com>
2023-06-09 11:17:07 -0400

    Create DartPrac.dart

---
+++ DartPrac.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  // DATATYPES: 
+  
+  String firstName = "Ron"; // string datatype
+  int x = 7; // integer number
+  double gpa = 3.50; // decimal number
+  bool isMale = true; // boolean is a true or false value
+
+  print("My name is ${firstName}");
+  print("My age is ${x}");
+  print("My current gpa is ${gpa}");
+  print("The statement that I am a male is ${isMale}");
+  
+  
+  
+}
